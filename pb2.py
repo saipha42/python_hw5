8794e51ddbe65d12c29c123f9c8bfc853a9af8ad
Sai Marn Pha <saimarnpha@mtu.edu.mm>
2023-08-12 23:31:40 +0700

    Completed fine

--- pb2.py
+++ pb2.py
@@ -1,6 +1,6 @@
 import turtle
 
-gap = 20
+gap = 30
 
 calendar_width = 200
 calendar_high = 150
@@ -99,52 +99,86 @@ def draw_cal_week_head() :
 month_31 = [1,3,5,7,8,10,12]
 
 
+
 def wheather_to_write_day(day, month) :
     if type(day) != int :
         turtle.write(day)
     else :
-        if day == 0 :
-            print()
-            
         if month in month_31 :
-            if day <= 31 :
-                
+            if day <= 31 and day > 0 :
                 turtle.write(day)
-                
-
         else :
-            if month == 2 :
+            if month == 2 and day > 0 :
                 if day <= 28 :
-                       
                         turtle.write(day)
-                        
             else :
-                if day <= 30 :
-                    
+                if day <= 30 and day > 0 :
                     turtle.write(day)
-                    
 
 
-def draw_cal(row, col, month) :
+def should_count_first_day(day, month) :
+
+    if month in month_31 :
+        if day <= 31 :
+            return True
+    else :
+        if month == 2 :
+            if day <= 28 :
+                    return True
+        else :
+            if day <= 30 :
+                return True
+    return False
+
+def draw_cal(row, col, month, first_day) :
     day = 0
     x = -430
     if col > 1 :
         x = -430 + ((calendar_width + gap) * (col -1))
 
-    for row in range(1, 6) :
+
+    for row in range(1, 7) :
 
         turtle.goto(x, turtle.ycor() - box_high)
         turtle.pendown()
+
+
         for col in range(1, 8) :
+            
             #logic when to start count the day
-            day += 1
+            # day += 1
+            print("frist day : ", first_day, " Day : ", day , " col : ", col)
+
+            if day == 0 :
+                if col == first_day :
+                    day += 1
+                    
+            elif day > 0 and should_count_first_day(day, month):
+                day += 1
+            if day > 0 and should_count_first_day(day, month) :
+                first_day +=1
+
+            if (first_day > 7) :
+                first_day = 1
             #end of logic
-            draw_cal_box(day, month)
-            turtle.penup()
-            turtle.forward(box_len)
-        
+            
+            if row == 6 :
+                if month == 4 or month == 7 or month == 12 :
+                    draw_cal_box(day, month)
+                    turtle.penup()
+                    turtle.forward(box_len)
+                else :
+                    continue
+            else :
+                draw_cal_box(day, month)
+                turtle.penup()
+                turtle.forward(box_len)
+
+
+    return first_day
 
 month = 0
+first_day_of_the_month = 1
 #calendar grid row loop
 for row in range(0, 3) :
 
@@ -155,9 +189,10 @@ for row in range(0, 3) :
     for col in range(1, 5):
         month += 1
         pre_turtle(row, col)
-        draw_cal_head(2)
+        draw_cal_head(month)
         draw_cal_week_head()
-        draw_cal(row, col, month)
+
+        first_day_of_the_month= draw_cal(row, col, month, first_day_of_the_month)
     
 
 turtle.done()
